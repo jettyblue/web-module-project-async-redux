2ce595e5a75eb181d6af1d669d871917ad27224c
unknown <lindsay.kelly15@gmail.com>
2022-02-16 17:59:36 -0500

    receiving api data

--- src/App.js
+++ src/App.js
@@ -1,7 +1,24 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 import './App.css';
 
 function App() {
+  const [data, setData] = useState([]);
+  const [error, setError] = useState(null);
+
+
+  useEffect(() => 
+    axios.get("http://www.omdbapi.com/?t=superbad&apikey=59398ee2")
+    .then(res => {
+      console.log(res)
+    })
+    .catch(err => {
+      console.error(err)
+    }) 
+  )
+
+
+
   return (
     <div className="App">
       Async Redux Project
