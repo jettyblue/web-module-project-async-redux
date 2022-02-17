160f0bde07759ad492c1376347c1a1021fdbbc47
unknown <lindsay.kelly15@gmail.com>
2022-02-16 19:57:13 -0500

    files added, page has gifts and text showing up on page

--- src/App.js
+++ src/App.js
@@ -1,29 +1,46 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
+
+import GifList from './components/GifList';
+import GifForm from './components/GifForm';
+import data from './data/gifs';
 import './App.css';
 
-function App() {
-  const [data, setData] = useState([]);
-  const [error, setError] = useState(null);
+function App(props) {
+  const gifs = data;
+  const loading = false;
+  const error = '';
+
+  // const [dogData, setDogData] = useState([]);
+  // const [error, setError] = useState(null);
 
 
-  useEffect(() => 
-    axios.get("http://www.omdbapi.com/?t=superbad&apikey=59398ee2")
-    .then(res => {
-      console.log(res)
-    })
-    .catch(err => {
-      console.error(err)
-    }) 
-  )
-
+  // useEffect(() => 
+  //   axios.get("https://api.giphy.com/v1/gifs/search?api_key=rdqOVrR8QiWAO6A5R3i5yuQqIZBlQ1Af&q=dogs")
+  //   .then(res => {
+  //     console.log(res.data.data)
+  //   })
+  //   .catch(err => {
+  //     console.error(err)
+  //   }) 
+  // )
 
 
   return (
     <div className="App">
-      Async Redux Project
+      <h1>Async Redux Project</h1>
+      <h2> Search for Gifs</h2>
+      <GifForm />
+
+      {
+        loading? <h3>Loading...</h3> : <GifList gifs={gifs} />
+      }
+
     </div>
   );
 }
 
 export default App;
+
+// rdqOVrR8QiWAO6A5R3i5yuQqIZBlQ1Af
+
---
+++ src/data/gifs.js
@@ -0,0 +1,72 @@
+const data = [
+    {
+        "type": "gif",
+        "id": "2C2qwckZzyiz8UzvzK",
+        "url": "https://giphy.com/gifs/bestfriends-save-them-all-best-friends-animal-society-bfas-2C2qwckZzyiz8UzvzK",
+        "username": "bestfriends",
+        "source": "bestfriends.org",
+        "title": "Dog What GIF by Best Friends Animal Society",
+        "rating": "g",
+        "import_datetime": "2021-01-07 19:57:39",
+        "trending_datetime": "0000-00-00 00:00:00",
+        "images": {
+            "original": {
+                "url": "https://media3.giphy.com/media/2C2qwckZzyiz8UzvzK/giphy.gif?cid=87682957dwteqjnxswbofzvfw8gahlyz1g15e8tl4gjbzh2y&rid=giphy.gif&ct=g",
+                "hash": "48552e533b35c0e735e2125b1febac7c"
+            }
+        }
+    },
+    {
+        "type": "gif",
+        "id": "HzkZeQM6aHcodITVXf",
+        "url": "https://giphy.com/gifs/viralhog-viral-hog-synchronized-tail-wagging-HzkZeQM6aHcodITVXf",
+        "username": "viralhog",
+        "source": "",
+        "title": "Dog GIF by ViralHog",
+        "rating": "g",
+        "import_datetime": "2021-04-13 23:41:31",
+        "trending_datetime": "0000-00-00 00:00:00",
+        "images": {
+            "original": {
+                "url": "https://media0.giphy.com/media/HzkZeQM6aHcodITVXf/giphy.gif?cid=87682957dwteqjnxswbofzvfw8gahlyz1g15e8tl4gjbzh2y&rid=giphy.gif&ct=g",
+                "hash": "816caab2d44cfb628e11b2ca372a66be"
+            }
+        }
+    },
+    {
+        "type": "gif",
+        "id": "MtsBHd0UsiH7t6twq0",
+        "url": "https://giphy.com/gifs/viralhog-viral-hog-beagle-comforted-by-baby-blankets-MtsBHd0UsiH7t6twq0",
+        "username": "viralhog",
+        "source": "http://viralhog.com",
+        "title": "Dog GIF by ViralHog",
+        "rating": "pg",
+        "import_datetime": "2021-08-09 21:14:23",
+        "trending_datetime": "0000-00-00 00:00:00",
+        "images": {
+            "original": {
+                "url": "https://media1.giphy.com/media/MtsBHd0UsiH7t6twq0/giphy.gif?cid=87682957dwteqjnxswbofzvfw8gahlyz1g15e8tl4gjbzh2y&rid=giphy.gif&ct=g",
+                "hash": "b2331c36b49cafaad06558552f4524c0"
+            },
+        },
+    },
+    {
+        "type": "gif",
+        "id": "iDJQRjTCenF7A4BRyU",
+        "url": "https://giphy.com/gifs/viralhog-cute-scary-chucky-iDJQRjTCenF7A4BRyU",
+        "username": "viralhog",
+        "source": "viralhog.com",
+        "title": "Dog Halloween GIF by ViralHog",
+        "rating": "pg",
+        "import_datetime": "2019-10-21 17:51:42",
+        "trending_datetime": "2019-11-01 00:00:08",
+        "images": {
+            "original": {
+                "url": "https://media0.giphy.com/media/iDJQRjTCenF7A4BRyU/giphy.gif?cid=87682957dwteqjnxswbofzvfw8gahlyz1g15e8tl4gjbzh2y&rid=giphy.gif&ct=g",
+                "hash": "8dc0fe4d03543408d3971a3067506958"
+            }
+        }
+    },  
+]
+
+export default data;
